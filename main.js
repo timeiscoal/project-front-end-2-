d0cc4da05e42ea8654cf751729fa71abff3a199c
sunmi-park <ppssmm0331@gmail.com>
2022-11-25 21:43:15 +0900

    메인페이지 테스트

---
+++ main.js
@@ -0,0 +1,41 @@
+console.log('ㅇㅁㅇ')
+
+
+window.onload = async function loadArticles(){
+    const response = await fetch('http://127.0.0.1:8000/article/',{
+        method:'GET',
+        headers:{
+            "Authorization": localStorage.getItem("access")
+        }
+    })
+
+
+    response_json = await response.json()
+   
+    image_json = response_json[0]
+    //console.log('제이슨?',response_json)
+    //console.log('이미지 왜 안나와',image_json) 
+    const image_container = document.getElementById('user-box')
+   //console.log(image_container)
+    
+        for (i = 0; i <  response_json.length; i++){
+        
+             const image =  response_json[i]['image']
+       
+
+            const new_image = `<div class='thumbnail' id="user-box">
+            <div class='photoContainer'>
+              <a href='#'>
+                <img src='http://127.0.0.1:8000/${image}'/>
+                <div class='photoInfo'>
+                  <h3>"Loneliness of autumn"</h3>
+                  <span class='paintingDate'>Leonid Afremov</span>
+                </div>          
+              </a>
+            </div>
+          </div>`
+
+          image_container.insertAdjacentHTML("beforeend",new_image)
+        }
+      
+    }
