a2bdb106119e970d3ae1459b53ee2b17e702ea99
timeiscoal <timeiscoal@gmail.com>
2022-11-26 01:28:20 +0900

    충돌 해제 중

---
+++ main1.js
@@ -0,0 +1,46 @@
+console.log("ㅇㅁㅇ");
+
+window.onload = async function loadArticles() {
+  const response = await fetch("http://127.0.0.1:8000/post/article/", {
+    method: "GET",
+    headers: {
+      Authorization: localStorage.getItem("access"),
+    },
+  });
+
+
+    response_json = await response.json();
+
+
+    image_json = response_json[0];
+
+    const image_container = document.getElementById("user-box");
+
+  
+
+    for (i = 0; i < response_json.length; i++) {
+    const image = response_json[i]["picture"];
+
+    const new_image = `<div class='thumbnail' id="user-box">
+            <div class='photoContainer'>
+              <a href='#'>
+                <img src='http://127.0.0.1:8000/${image}'/>
+                <div class='photoInfo'>
+                  <h3>"Loneliness of autumn"</h3>
+                  <span class='paintingDate'>Leonid Afremov</span>
+                </div>          
+              </a>
+            </div>
+          </div>`;
+
+          image_container.insertAdjacentHTML("beforeend",new_image)
+        }
+      
+      } 
+
+
+  async function imageUload(){
+    console.log('버튼 클릭')
+      window.location.href="http://127.0.0.1:5500/image.html"
+
+    }
